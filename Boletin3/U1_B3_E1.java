8cd5881f5f29f45947323527432d808eb749d979
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 11:00:55 +0100

    version1

---
+++ Boletin3/U1_B3_E1.java
@@ -0,0 +1,11 @@
+class U1_B3_E1{
+    public static void main(String[] args){
+        System.out.println("Tipo\tMin.\t\tMax.");
+        System.out.println("int\t"+Integer.MIN_VALUE+"\t"+Integer.MAX_VALUE);
+        System.out.println("byte\t"+Byte.MIN_VALUE+"\t\t"+Byte.MAX_VALUE);
+        System.out.println("short\t"+Short.MIN_VALUE+"\t\t"+Short.MAX_VALUE);
+        System.out.println("float\t"+Float.MIN_VALUE+"\t\t"+Float.MAX_VALUE);
+        System.out.println("double\t"+Double.MIN_VALUE+"\t"+Double.MAX_VALUE);
+        }}
+    
+
