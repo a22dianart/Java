4f3a1c8599f562a0993473a0a436682fd2615f70
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 12:20:32 +0100

    version1

---
+++ Boletin3/U1_B3_E6.java
@@ -0,0 +1,6 @@
+class U1_B3_E6{
+public static void main(String[] args) {
+float miFloat=10.0f; //Ao poñer aquí unha f detrás do número funciona
+double miDouble=10.0;
+}}
+//isto débese a que os números que non son enteiros java enténdeos como double por defecto.
