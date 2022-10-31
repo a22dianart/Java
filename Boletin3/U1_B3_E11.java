bcdb09d47a2a9c7e9a8a3adb75f84268a03e5b35
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 12:23:23 +0100

    version1

---
+++ Boletin3/U1_B3_E11.java
@@ -0,0 +1,11 @@
+class U1_B3_E11{
+    public static void main(String[] args){
+        String nombre="Juán López Salvado";
+        char sexo='M';
+        short edad=49;//supoñemos que pode ter máis de 127 
+        float peso=70.8f; //xa que por defecton usa o double pero este ocupa máis 
+        System.out.println("nome: "+ nombre);
+        System.out.println("sexo: "+ sexo);
+        System.out.println("idade: "+ edad);
+        System.out.println("peso: "+ peso);
+}}
