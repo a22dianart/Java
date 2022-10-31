8d82dd3686bfd206d50830b354d1427a8d739a7b
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 12:21:19 +0100

    version1

---
+++ Boletin3/U1_B3_E7.java
@@ -0,0 +1,8 @@
+//O seguinte código se o sacásemos de ser un comentario daría erro
+/*
+class App{
+    public static void main(String[] args){
+        System.out.println("Esto no es una secuencia de escape \p y da error");
+}}
+*/
+//Esto é debido a que "/" hai que usalo cun caracter especial que signifique algo e p non está incluído.
