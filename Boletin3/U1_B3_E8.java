532ddf0ae0eaf738a6e6d269c1fb34504c2c0c4d
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 12:21:46 +0100

    version1

---
+++ Boletin3/U1_B3_E8.java
@@ -0,0 +1,4 @@
+class U1_B3_E8{
+    public static void main(String[] args){
+        System.out.println("si pongo \n no se imprime la barra y la n"); //secuencia de escape que significa salto de liña
+}}
