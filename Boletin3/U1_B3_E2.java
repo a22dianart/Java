6641cfcaf8ed7a5f8d77055bb51283414d24ffb2
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 11:08:01 +0100

    version1

---
+++ Boletin3/U1_B3_E2.java
@@ -0,0 +1,4 @@
+// ESTO HABERÍA QUE MIRALO CO JSHELL
+/* Se empezamos a asignarlle un número e este comeza por 0, estamos indicándolle
+   que imos a escribir o número en octal, pero en octal a cifra 8 non existe, por
+   iso da erro. */ 
