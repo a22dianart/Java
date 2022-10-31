00b5813adc8a82eb2db4220993a38e7692cfd789
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 12:24:17 +0100

    version1

---
+++ Boletin3/U1_B3_E12.java
@@ -0,0 +1,14 @@
+/*
+178-> Tipo int (base decimal)
+2L-> Tipo long (base decimal)
+077L-> Tipo long (base octal)
+0xBAACL-> Tipo long (base hexadecimal)
+32.266D-> Tipo double (base decimal)
+87.363F-> Tipo float (base decimal)
+'c'-> Tipo char
+'\t'-> Tipo char
+true-> Tipo boolean
+false-> Tipo boolean
+'\u00E1'-> Tipo char
+"hola"-> String (non é un primitivo, é un obxecto)
+*/
