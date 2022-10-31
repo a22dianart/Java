a70737758e214345ed55f02d45dc1cc628eedf90
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-31 10:35:32 +0100

    version1

---
+++ Boletin2/U1_B2_E1.java
@@ -0,0 +1,9 @@
+class U1_B2_E1{
+	public static void main(String[] args){
+		int lado;
+		int area;
+		lado=2;
+		area=lado*lado;
+		System.out.println("A area dun cadrado de lado " +lado+ " e " +area);
+	}
+}
